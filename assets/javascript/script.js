ecf8f9961f8c5c45c6095c620086a7a245ea3ba4
Sal Gonzalez <imdone151@gmail.com>
2023-09-24 14:56:52 -0700

    added code to not allow a blank button and duplicate buttons. Also moved functions around so it works properly

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,14 +13,14 @@ cityStorage();
 
 function currentWeather(city) {
     var catURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + '&units=imperial';
-   
-    
+
+
     fetch(catURL)
         .then(function (response) {
             return response.json()
         })
         .then(function (data) {
-            var weatherIcon =  `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
+            var weatherIcon = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
             var imageHTML = `<img src="${weatherIcon}" alt="weather icon" width="40" height="46">`
             console.log(data);
             var cityName = data.name;
@@ -40,62 +40,65 @@ function currentWeather(city) {
                 .then(function (data) {
                     fiveDayEl.innerHTML = '';
                     for (var i = 0; i < data.list.length; i = i + 8) {
-                    console.log(data.list[i]);
-                    var card1 = document.createElement('h5');
-                    var card2 = document.createElement('p');
-                    var card3 = document.createElement('p')
-                    var card4 = document.createElement('p')
-                    var weatherIcon2 =  `https://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`
-                    var imageHTML2 = `<img src="${weatherIcon2}" alt="weather icon" width="40" height="46">`
-                    card1.setAttribute('class', 'col-2 card');
-                    card1.innerHTML = dayjs(data.list[i+1].dt_txt).format('M/D/YYYY') + imageHTML2;
-                    card2.textContent = 'temp: ' + data.list[i].main.temp;
-                    card3.textContent = 'Wind: ' + data.list[i].wind.speed;
-                    card4.textContent = 'Humidity: ' + Math.floor(data.list[i].main.humidity);
-                    console.log(card2);
-                    fiveDayEl.appendChild(card1);
-                    card1.appendChild(card2);
-                    card1.appendChild(card3);
-                    card1.appendChild(card4);
-                    
+                        console.log(data.list[i]);
+                        var card1 = document.createElement('h5');
+                        var card2 = document.createElement('p');
+                        var card3 = document.createElement('p')
+                        var card4 = document.createElement('p')
+                        var weatherIcon2 = `https://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`
+                        var imageHTML2 = `<img src="${weatherIcon2}" alt="weather icon" width="40" height="46">`
+                        card1.setAttribute('class', 'col-2 card');
+                        card1.innerHTML = dayjs(data.list[i + 1].dt_txt).format('M/D/YYYY') + imageHTML2;
+                        card2.textContent = 'temp: ' + data.list[i].main.temp;
+                        card3.textContent = 'Wind: ' + data.list[i].wind.speed;
+                        card4.textContent = 'Humidity: ' + Math.floor(data.list[i].main.humidity);
+                        console.log(card2);
+                        fiveDayEl.appendChild(card1);
+                        card1.appendChild(card2);
+                        card1.appendChild(card3);
+                        card1.appendChild(card4);
+
 
 
                     }
-                    
+
                 })
         })
 }
+
+
+function cityStorage() {
+    savedListEl.innerHTML = '';
+    var cities = JSON.parse(localStorage.getItem('cities'));
+    if (cities)
+        for (var i = 0; i < cities.length; i++) {
+            var recentSearches = document.createElement('button')
+            recentSearches.textContent = cities[i];
+            recentSearches.setAttribute('class', 'btn btn-secondary col-12 my-1');
+            savedListEl.appendChild(recentSearches);
+        }
+}
+
 
 searchBtnEl.addEventListener('click', function (event) {
     event.preventDefault();
     city = searchBarEl.value;
     currentWeather(city);
-    var cities = JSON.parse(localStorage.getItem('cities'))
-    if (cities) {
-        cities.push(city)
-        localStorage.setItem('cities', JSON.stringify(cities))
-    } else {
-        cities = []
-        cities.push(city)
-        localStorage.setItem('cities', JSON.stringify(cities))
-    }
-    if (!searchBarEl.value) return;
-    localStorage.getItem(city);
+    var cities = JSON.parse(localStorage.getItem('cities')) || [];
+
+    if (city === '') {
+        return;
+    } else
+        if (!cities.includes(city)) {
+            cities.push(city);
+            localStorage.setItem('cities', JSON.stringify(cities));
+        }
+
+    localStorage.setItem('city', city);
     cityStorage();
-    
-    
 });
 
-function cityStorage () {
-        savedListEl.innerHTML = '';
-        var cities = JSON.parse(localStorage.getItem('cities'))
-        if (cities)
-        for (var i = 0; i < cities.length; i++) {
-        var recentSearches = document.createElement('button')
-        recentSearches.textContent = cities[i];
-        savedListEl.appendChild(recentSearches);
-        }
-}
+
 
 savedListEl.addEventListener('click', function (event) {
     if (event.target.tagName === 'BUTTON') {
